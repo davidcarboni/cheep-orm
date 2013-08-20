f1050c5cc25ca89f42e9f0b2d593ec8a2ef98ce8
David <david@carboni.co>
2013-08-20 08:55:11 +0100

    Trying to configure database access on Travis.

--- src/test/java/net/jirasystems/cheeporm/Database.java
+++ src/test/java/net/jirasystems/cheeporm/Database.java
@@ -9,6 +9,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import org.apache.commons.lang.StringUtils;
+
 /**
  * @author david
  * 
@@ -17,78 +19,101 @@ public class Database {
 
 	public static boolean useTransaction = true;
 
-	public static String databaseCreationUser = "root";
-	public static String databaseCreationPassword = "password";
-	public static String testDatabaseName = "cheeporm_test";
+	public static String databaseCreationUser;
+	public static String databaseCreationPassword;
+	public static String testDatabaseName;
 
-	public static String testUser = "cheeporm";
-	public static String testPassword = "password";
+	public static String testUser;
+	public static String testPassword;
 
 	private static Reflection reflection = new Reflection();
 
-	public static void beforeSuite() throws ClassNotFoundException, SQLException {
+	public static void beforeSuite() throws ClassNotFoundException,
+			SQLException {
 
 		// Attempt to load configuration values:
 		try {
 			Properties properties = DatabaseProperties.getProperties();
 
-			databaseCreationUser = properties.getProperty("databaseCreationUser", "root");
-			databaseCreationPassword = properties.getProperty("databaseCreationPassword", "password");
-			testDatabaseName = properties.getProperty("testDatabaseName", "cheeporm_test");
+			databaseCreationUser = properties.getProperty(
+					"databaseCreationUser", "");
+			databaseCreationPassword = properties.getProperty(
+					"databaseCreationPassword", "");
+			testDatabaseName = properties.getProperty("testDatabaseName",
+					"cheeporm");
 
-			testUser = properties.getProperty("testUser", "cheeporm");
-			testPassword = properties.getProperty("testPassword", "password");
+			testUser = properties.getProperty("testUser", "travis");
+			testPassword = properties.getProperty("testPassword", "");
 		} catch (RuntimeException e) {
-			System.out.println("Unable to load database properties: " + e.getMessage());
+			System.out.println("Unable to load database properties: "
+					+ e.getMessage());
 			System.out.println("Using default values");
 		}
 
-		Connection connection;
-
-		Class.forName("com.mysql.jdbc.Driver");
-
-		// Set up the database
-
-		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/", databaseCreationUser,
-				databaseCreationPassword);
-
-		PreparedStatement createDatabase = connection.prepareStatement("create database if not exists "
-				+ testDatabaseName + ";");
-		try {
-			createDatabase.execute();
-		} finally {
-			createDatabase.close();
-		}
-
-		PreparedStatement grant = connection.prepareStatement("grant all on " + testDatabaseName + ".* to " + testUser
-				+ "@localhost identified by '" + testPassword + "';");
-		try {
-			grant.execute();
-		} finally {
-			grant.close();
-		}
-		connection.close();
-	}
-
-	public static void afterSuite() throws ClassNotFoundException, SQLException {
-
-		if (useTransaction) {
-
+		// Create the database if credentials aren't specified in
+		// database.properties
+		// By default Travis-ci provides a database:
+		if (StringUtils.isNotBlank(databaseCreationUser)) {
 			Connection connection;
 
-			// Drop the database
+			Class.forName("com.mysql.jdbc.Driver");
 
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/", databaseCreationUser,
+			// Set up the database
+
+			connection = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/", databaseCreationUser,
 					databaseCreationPassword);
 
-			PreparedStatement createDatabase = connection.prepareStatement("drop database if exists "
-					+ testDatabaseName + ";");
+			PreparedStatement createDatabase = connection
+					.prepareStatement("create database if not exists "
+							+ testDatabaseName + ";");
 			try {
 				createDatabase.execute();
 			} finally {
 				createDatabase.close();
 			}
+
+			PreparedStatement grant = connection
+					.prepareStatement("grant all on " + testDatabaseName
+							+ ".* to " + testUser
+							+ "@localhost identified by '" + testPassword
+							+ "';");
+			try {
+				grant.execute();
+			} finally {
+				grant.close();
+			}
 			connection.close();
+		}
+	}
+
+	public static void afterSuite() throws ClassNotFoundException, SQLException {
+
+		// Create the database if credentials aren't specified in
+		// database.properties
+		// By default Travis-ci provides a database:
+		if (StringUtils.isNotBlank(databaseCreationUser)) {
+
+			if (useTransaction) {
+
+				Connection connection;
+
+				// Drop the database
+
+				connection = DriverManager.getConnection(
+						"jdbc:mysql://localhost:3306/", databaseCreationUser,
+						databaseCreationPassword);
+
+				PreparedStatement dropDatabase = connection
+						.prepareStatement("drop database if exists "
+								+ testDatabaseName + ";");
+				try {
+					dropDatabase.execute();
+				} finally {
+					dropDatabase.close();
+				}
+				connection.close();
+			}
 		}
 	}
 
@@ -99,8 +124,9 @@ public class Database {
 	 *             If creating the Connection fails.
 	 */
 	public static Connection beforeTest(Object bean) throws Exception {
-		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + testDatabaseName,
-				testUser, testPassword);
+		Connection connection = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/" + testDatabaseName, testUser,
+				testPassword);
 
 		// Create the test table if necessary
 
@@ -111,8 +137,9 @@ public class Database {
 
 		// Remove any leftover test data
 
-		PreparedStatement deleteFromTable = connection.prepareStatement("delete from " + reflection.getTable(bean)
-				+ ";");
+		PreparedStatement deleteFromTable = connection
+				.prepareStatement("delete from " + reflection.getTable(bean)
+						+ ";");
 		try {
 			deleteFromTable.execute();
 		} finally {
